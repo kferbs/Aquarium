bead8ffcc06ea61c0292b4a8c70409f1e0095ec5
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-13 14:43:03 -0500

    final commit

--- src/main/java/com/example/nonprofit/HomeController.java
+++ src/main/java/com/example/nonprofit/HomeController.java
@@ -9,6 +9,40 @@ public class HomeController {
     public String index(){
         return "index";
     }
+    @RequestMapping("/memberpage")
+    public String memberPage(){
+        return "memberpage";
+    }
+    @RequestMapping("/visitpage")
+    public String visitPage(){
+        return "visitpage";
+    }
+    @RequestMapping("/contactpage")
+    public String contactPage(){
+        return "contactpage";
+    }
+    @RequestMapping("/animals")
+    public String animalPage(){
+        return "animals";
+    }
+    @RequestMapping("/octopus")
+    public String octopus(){
+        return "octopus";
+    }
+    @RequestMapping("/seaotters")
+    public String seaotters(){
+        return "seaotters";
+    }
+    @RequestMapping("/penguins")
+    public String penguins(){
+        return "penguins";
+    }
+    @RequestMapping("/sharkcam")
+    public String sharkcam(){
+        return "sharkcam";
+    }
+
+
 
 }
 
