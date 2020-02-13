077816617061b3af7d34bcc733b0720a82ed29de
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-13 10:06:54 -0500

    first commit

---
+++ src/main/java/com/example/nonprofit/NonprofitApplication.java
@@ -0,0 +1,13 @@
+package com.example.nonprofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonprofitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonprofitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nonprofit/NonprofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nonprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonprofitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
